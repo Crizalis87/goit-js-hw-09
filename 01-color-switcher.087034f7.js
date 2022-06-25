b746de352d1f6ca305ba330afec360308642b325
Crizalis87 <102592971+Crizalis87@users.noreply.github.com>
2022-06-25 13:28:49 +0000

    Deploying to gh-pages from @ Crizalis87/goit-js-hw-09@ead77efbf5ba26f4b307196417eac8f01794a65e 🚀

---
+++ 01-color-switcher.087034f7.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),o=document.querySelector("button[data-stop]");t.addEventListener("click",(function(){setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)&&(t.disabled=!0,o.disabled=!1)})),o.addEventListener("click",onStopButtonClick)}();
+//# sourceMappingURL=01-color-switcher.087034f7.js.map
---
+++ 01-color-switcher.9ff2c1fd.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let o=null;t.addEventListener("click",(function(){o=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),o&&(t.disabled=!0,e.disabled=!1)})),e.addEventListener("click",onStopButtonClick);
+//# sourceMappingURL=01-color-switcher.9ff2c1fd.js.map
