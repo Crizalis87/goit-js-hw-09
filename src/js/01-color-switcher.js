ead77efbf5ba26f4b307196417eac8f01794a65e
Dmitriy Yermolaiev <102592971+Crizalis87@users.noreply.github.com>
2022-06-25 16:28:06 +0300

    update task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,20 @@
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
+let timerId = null;
 
+function onStartColorClick() {
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  if (timerId) {
+    startButton.disabled = true;
+    stopButton.disabled = false;
+  }
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startButton.addEventListener('click', onStartColorClick);
+stopButton.addEventListener('click', onStopButtonClick);
